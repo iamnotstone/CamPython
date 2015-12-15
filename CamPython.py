19c3273b79786e5e7f0828536d303b9cbb11ef52
iamnotstone <iamnotstone@sina.com>
2015-12-15 11:25:10 +0900

    Update CamPython.py

--- CamPython.py
+++ CamPython.py
@@ -1,4 +1,53 @@
+# -*- coding: utf-8 -*-
 from VideoCapture import Device
+import wx
+from PIL import Image
 
-cam = Device(1)
-cam.saveSnapshot('image.jpg')
+SIZE = (640, 480)
+
+def get_image():
+    cam = Device(0)
+    return cam.getImage(1)
+
+def pil_to_wx(image):
+    width, height = image.size
+    buffer = image.convert('RGB').tostring()
+    bitmap = wx.BitmapFromBuffer(width, height, buffer)
+    return bitmap
+
+class Panel(wx.Panel):
+    def __init__(self, parent):
+        super(Panel, self).__init__(parent, -1)
+        self.SetSize(SIZE)
+        self.SetBackgroundStyle(wx.BG_STYLE_CUSTOM)
+        self.Bind(wx.EVT_PAINT, self.on_paint)
+        self.update()
+    def update(self):
+        self.Refresh()
+        self.Update()
+        wx.CallLater(100, self.update)
+    def create_bitmap(self):
+        image = get_image()
+        bitmap = pil_to_wx(image)
+        return bitmap
+    def on_paint(self, event):
+        bitmap = self.create_bitmap()
+        dc = wx.AutoBufferedPaintDC(self)
+        dc.DrawBitmap(bitmap, 0, 0)
+
+class Frame(wx.Frame):
+    def __init__(self):
+        style = wx.DEFAULT_FRAME_STYLE & ~wx.RESIZE_BORDER & ~wx.MAXIMIZE_BOX
+        super(Frame, self).__init__(None, -1, 'Camera Viewer', style=style)
+        panel = Panel(self)
+        self.Fit()
+
+def main():
+    app = wx.App()
+    frame = Frame()
+    frame.Center()
+    frame.Show()
+    app.MainLoop()
+
+if __name__ == '__main__':
+    main()
